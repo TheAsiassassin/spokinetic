3b7a2903138889f539a94ccdff817c80914a3a7c
Steven McConnell <stevendanielmcconnell@gmail.com>
2020-04-19 15:54:39 -0700

    Design change on greating page

--- tabris/src/index.jsx
+++ tabris/src/index.jsx
@@ -1,15 +1,17 @@
 import {Button, TextView, contentView, AlertDialog, TextInput, Row, CollectionView, 
         TabFolder, Tab, ImageView, Stack, Page, NavigationView, ListView, Cell, Action, 
-        SearchAction, ScrollView, Composite, drawer, Popover, StackLayout
+        SearchAction, ScrollView, Composite, drawer, Popover, StackLayout, RowLayout
       } from 'tabris';
 import {CreateEventPage} from './create-event';
 import {SignUpPage} from './sign-up';
 import {SignInPage} from './sign-in';
+import {EventNotifyPage} from './eventsNotificationPage';
 
-/*
-* Do a method call to initialize these variables eventually
-* Call will be made to events database to pull top events
-* For now set them to stock images
+/**
+ * Add In Later Iteration
+ * Do a method call to initialize these variables eventually
+ * Call will be made to events database to pull top events
+ * For now set them to stock images
 */
 
 // Temporary(?) boolean to track for new users
@@ -114,29 +116,58 @@ drawer.append(
 const pageRef = $(Page).only(); 
 //  '$'  is equivalent to 'tabris.contentView.find'  
 
-if(!signedInBoolean) {
-  showLanding();
-}
+checkShowLanding();
 
+function checkShowLanding(){
+  if(!signedInBoolean) {
+    showLanding();
+  }
+}
+/**
+ *  Opens a "Create New Event" page
+ */
 function openCreatePage() {
   $(NavigationView).only().append(
     <CreateEventPage />
   );
 }
-
+/**
+ * Initial page user sees. Prompts user to sign up.
+ * 
+ * TO DO:
+ * add padding to ScrollView 
+ * make sizing more generic for different screen sizes
+ * bypass this page if already signed in
+ * include info in the white space below the "get started button"
+ * make stack section scrollable: see eventPage.jsx.
+ * pull out common setters, (replicated code) 
+ * (i.e. all text is white, set it outside of popover with a reference to the entire popover tag)
+ */
 function showLanding() {
   const popover = Popover.open(
-    <Popover width={300} height={400}>
-      <ScrollView stretch layout={new StackLayout({spacing: 16, alignment: 'stretchX'})} padding={16}>
-        <Button style='outline' right strokeColor='blue' textColor='blue' onSelect={() => showSignIn(popover)}>Sign in</Button>
-        <TextView centerX top={50} font='bold 36px'>Welcome!</TextView>
-        <TextView centerX font='24px'>All your local events, right</TextView>
-        <TextView centerX top={5} font='24px'>in the palm of your hand</TextView>
-        <Button centerX top={64} style='flat' background='blue' font='bold 24px' textColor='white' onSelect={() => showSignUp(popover)}>Get Started</Button>
-      </ScrollView>
+    <Popover stretchX >
+  
+        <Stack background='#79a6e1'  stretchX height={550}>
+          <ScrollView top background='#234' stretchX height={72} direction='horizontal' layout={new RowLayout({alignment: 'stretchY'})}>
+            <TextView left textColor='white' font='bold 35px'>  Spokinetic    </TextView>
+            <Button style='text' textColor='white' font='bold 14px' onSelect={() => showSignIn(popover)}>SIGN IN</Button>
+            <TextView textColor='white'> | </TextView>
+            <Button style='text' textColor='white' font='bold 14px' onSelect={() => showSignUp(popover)}>SIGN UP</Button>
+          </ScrollView>
+          
+          <TextView centerX top={60} textColor='white' font='bold 40px'>Welcome!</TextView>
+          <TextView centerX textColor='white' font='bold 24px'>All your local events, right</TextView>
+          <TextView centerX top={5} textColor='white' font='bold 24px'>in the palm of your hand</TextView>
+          <Button centerX top={105} width={300} height={70} background='#77c666' style='flat' font='bold 18px' textColor='white' 
+          onSelect={() => showSignUp(popover)}>Get Started</Button>
+        </Stack>
+
+        <TextView bottom centerX textColor='#2f2f2f' font='bold 13px'>James Tollefson | Ian Oleson | John Petrovich | Steven McConnell</TextView>
+
     </Popover>
   );
 }
+
 
 function showSignUp(popover) {
   popover.close();
@@ -157,15 +188,36 @@ function showSignIn(popover) {
   );
   navigationView.set({toolbarVisible: false});
 }
+/*
+function showNotificationPage(popover){
+  popover.close();
+  $(NavigationView).only().append(
+    <EventNotifyPage />
+  );
+}*/
 
+/**
+ * Add In Later Iteration
+ * Home button hasn't been set.
+ * Navigates to main page
+ */
 function onHome(){
   pageRef.find('#initText').first(TextView).text = 'Home Pressed';
 }
-
+/**
+ * Add In Later Iteration
+ * Search bar has no search functionality
+ * Brings up keyboard and allows input
+ */
 function onSearch(){
 
 }
-
+/**
+ * Add In Later Iteration
+ * Ment to handle search bar input
+ * Save input for auto fill functionality or
+ * suggestions while typing
+ */
 function handleInput(){
 
 }
@@ -177,8 +229,12 @@ function handleLoad({target, error}) {
     //text: error ? 'Error' : 'Success'
   }).insertAfter(target);
 }
-
-
+/**
+ * Add In Later Iteration
+ * The onPage methods handle the three main events displayed
+ * on the main page. Each onPage method navigates to the event 
+ * page associated with the event displayed.
+ */
 function onPage1(){
   
 }
@@ -190,67 +246,28 @@ function onPage2(){
 function onPage3(){
 
 }
-
-function onPage4(){
-
-}
-
+/**
+ * Add In Later Iteration
+ * Incrementing the badge is currently for proof of concept
+ * OnEvents is associated with "events" tab. Tab may change
+ * to a notification tab. Events are visible in both calendar
+ * views.
+ */
 function onEvents(){
   eventNotifyInt ++;
   pageRef.find('#events').first(Tab).badge = eventNotifyInt; // increment badge val
-
-  //pageRef.find('#events').first(Tab).title = "this";
-  //pageRef.find('#eventTxt').first(TextView).text = eventNotifyInt;
 }
-
+/**
+ * Add In Later Iteration
+ * When selected the calendar page will be displayed
+ */
 function onCalendar(){
 
 }
-
+/**
+ * Add In Later Iteration
+ * When selected personal calendar page will be displayed
+ */
 function onMyCalendar(){
 
 }
-
-
-
-
-
-
-
-
-
-
-
-//onSelect={() => console.log('select Pay')} onReselect={() => console.log('reselect Pay')}
-// code snippet saved from testing Events, Calendar, and MyCalendar
-
-// unused functions ***************************
-function getImage(image) {
-  return 'resources/' + image + (device.platform === 'iOS' ? '-black-24dp@3x.png' : '-white-24dp@3x.png');
-}
-
-
-function initSearch(event){
-  //$(TextInput).only().text = 'Search';
-  contentView.append(
-    <TextView top='prev() 16' left='20%' text={event.text}/>
-  );
-  
-}
-
-function searchInput(ev) {
-  contentView.append(
-    //<TextView top='prev() 16' left='20%' text={ev.text}/>
-  );
-}
-
-function newButton(){
-  contentView.append(
-    <$>
-      <Button top onSelect={showText2} width='200'text='Hello World' background={[255, 128, 0]} ></Button>
-
-    </$>
-  );
-}
-
-//https://unsplash.com/photos/PMwu9gfCSbw image website
